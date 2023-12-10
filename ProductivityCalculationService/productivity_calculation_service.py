475c6d87f46182d5fe7af7b86c1fd32d3ee9c1c0
IsuruVindula <48341053+IsuruVindula@users.noreply.github.com>
2023-12-10 15:21:24 +0530

    Update productivity_calculation_service.py

--- ProductivityCalculationService/productivity_calculation_service.py
+++ ProductivityCalculationService/productivity_calculation_service.py
@@ -96,4 +96,4 @@ def ping():
 
 
 if __name__ == "__main__":
-    uvicorn.run("productivity_calculation_service:app",host="0.0.0.0",port=8002,reload=True)
+    uvicorn.run("productivity_calculation_service:app",host="0.0.0.0",port=8003,reload=True)
