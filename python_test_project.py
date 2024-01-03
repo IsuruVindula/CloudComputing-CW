15a9ea5598a7f35a0e5dddf01e5e693348e9ceb3
IsuruVindula <48341053+IsuruVindula@users.noreply.github.com>
2024-01-03 18:24:21 +0530

    Update python_test_project.py

--- python_test_project.py
+++ python_test_project.py
@@ -3,10 +3,10 @@ import requests
 
 ENDPOINT="http://16.171.58.216:5000"
 
-def test_home():
-    res = requests.get(ENDPOINT+'/')
-    assert res.status_code == 200
-    pass
+# def test_home():
+#     res = requests.get(ENDPOINT+'/')
+#     assert res.status_code == 200
+#     pass
 
 def test_get_github_commits_count_main():
     res = requests.get(ENDPOINT+'/get_github_commits_count_main?owner_username=IsuruVindula&repository_name=CloudComputing-CW&developer_username=IsuruVindula')
